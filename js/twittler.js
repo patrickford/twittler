1a0fa3cb47ecc15ac6a275edb07ddd5921fa06bc
Patrick Ford <patrickford@msn.com>
2016-08-20 13:58:02 -0700

    Full refactor

--- js/twittler.js
+++ js/twittler.js
@@ -1,56 +1,58 @@
-/*
- * NOTE: This file generates fake tweet data, and is not intended to be part of your implementation.
- * You can safely leave this file untouched, and confine your changes to index.html.
- */
+$(document).ready(function(){
 
-var refreshTweets = function(user){
-  var $tweetlist = $('#tweetlist');
-  $tweetlist.html('');
+  var refreshTweets = function(user){
+    var $tweetlist = $('#tweetlist');
+    $tweetlist.html('');
 
-  if (user) {
-    var index = streams.users[user].length - 1;
-  }
-  else {
-    var index = streams.home.length - 1;
-  }
-
-  while (index >=0) {
     if (user) {
-      var tweet = streams.users[user][index];
+      var index = streams.users[user].length - 1;
     }
     else {
-      var tweet = streams.home[index];
+      var index = streams.home.length - 1;
     }
- //   var $tweet = $('<div></div>');
- //   $tweet.text('@' + tweet.user + ': ' + tweet.message);
 
-    $singleTweet = $(''+ 
-      '<div class="singleTweet">' +
-        '<div class="userName"> <a href="#">' + '@' + tweet.user + '</a></div>' +
-        '<div class="tweetTime">' + jQuery.timeago(tweet.created_at) + '</div>' +
-        '<div class="tweetCopy"> <p>'+ tweet.message + '</p> </div>' + 
-      '</div>');
+    while (index >=0) {
+      if (user) {
+        var tweet = streams.users[user][index];
+      }
+      else {
+        var tweet = streams.home[index];
+      }
 
-    $singleTweet.appendTo('#tweetlist');
-    index -= 1;
-  }
-  createEventListener();
-};
+      $singleTweet = $(''+ 
+        '<div class="singleTweet">' +
+          '<div class="userName"> <a href="#">' + '@' + tweet.user + '</a></div>' +
+          '<div class="tweetTime">' + jQuery.timeago(tweet.created_at) + '</div>' +
+          '<div class="tweetCopy"> <p>'+ tweet.message + '</p> </div>' + 
+        '</div>');
 
-function sendTweet() {
-  var userInput = document.getElementById("draft").value;
-  writeTweet(userInput);
-  refreshTweets();
-  document.getElementById("draft").value = '';
-} 
+      $singleTweet.appendTo('#tweetlist');
+      index -= 1;
+    }
+  };
 
-function createEventListener() {
-  $('.userName').on('click', (function(e) {
-    event.preventDefault(e);
-    var $user = $(this).find('a').text();
-    $user = $user.slice(1);
-    refreshTweets($user);
+  $("#refresh").click(function() {
+    refreshTweets();
+  });
+
+  $("#myTweets").click(function() {
+    refreshTweets(visitor);
+  });
+
+  $("#newTweet").submit(function(e) {
+    e.preventDefault();
+    var userInput = $("#draft").val();
+    $("#draft").val('');    
+    writeTweet(userInput);
+    refreshTweets();
+  });
+
+  // Show tweets for only selected user
+  $(document).on('click', '.userName', (function(e) {
+    var user = $(this).find('a').text().slice(1);
+    refreshTweets(user);
   }));
-}
 
+  refreshTweets();
 
+})
